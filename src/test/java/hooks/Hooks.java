0461ed32e4771c2d7851a457507063b6c7a5f58a
MahmoudIbrahimMohamed95 <mahmoudibrahimmohamed9@gmail.com>
2026-09-09 08:47:38 +0300

    add test hooks

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,11 @@
+package hooks;
+import context.ScenarioContext;
+
+public class Hooks {
+
+    private final ScenarioContext scenarioContext;
+
+    public Hooks(ScenarioContext scenarioContext) {
+        this.scenarioContext = scenarioContext;
+    }
+}
