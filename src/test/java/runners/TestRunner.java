9f1fbe11c27b038237639b05a75a918339c51743
MahmoudIbrahimMohamed95 <mahmoudibrahimmohamed9@gmail.com>
2026-09-09 08:48:21 +0300

    configure cucumber runners

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,16 @@
+package runners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "classpath:features",
+        glue = {"stepDefinitions", "hooks"},
+        plugin = {
+                "pretty",
+                "summary",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests { }
